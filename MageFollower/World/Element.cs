a3fd06ef78d75a2b7b53ebaf11ff89c8b4219ae5
samuel grahame <samuel@intelogy.com.au>
2021-05-22 14:59:45 +1000

    added Elements

---
+++ MageFollower/World/Element.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MageFollower.World
+{
+    public enum Element
+    {
+        Fire,
+        Water,
+        Air,
+        Earth,
+        Disbander,
+        Fire_Light,
+        Water_Life,
+        Air_Ice,
+        Earth_Steel,
+        Fire_Dark,
+        Water_Poision,
+        Air_Lightning,
+        Earth_Crystal
+    }
+}
---
+++ MageFollower/World/Entity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MageFollower.World
+{
+    public class Entity
+    {
+        public string Id { get; set; }
+        public double Health { get; set; }
+
+
+        
+    }
+}
