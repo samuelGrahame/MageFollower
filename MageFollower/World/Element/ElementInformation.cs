4476a89f2eb7193cfa9763a7caeb635b1bd3a1ca
samuel grahame <samuel@intelogy.com.au>
2021-05-22 15:34:22 +1000

    added water ekenebt

--- MageFollower/World/Element/ElementInformation.cs
+++ MageFollower/World/Element/ElementInformation.cs
@@ -58,6 +58,27 @@ namespace MageFollower.World.Element
                 (ElementType.Earth_Steel, 0.75f),
                 (ElementType.Earth_Crystal, 0.75f)
             ));
+
+            AddTo(new ElementInformation()
+            {
+                Element = ElementType.Water
+            }.AddEffects( // Good Against
+                (ElementType.Fire, 1.25f),
+                (ElementType.Fire_Light, 1.25f),
+                (ElementType.Fire_Dark, 1.25f),
+                (ElementType.Disbander, 1.25f)
+            ).AddEffects( // Neutral Against
+                (ElementType.Air, 1f),
+                (ElementType.Water, 1f)
+            ).AddEffects( // Bad Against
+                (ElementType.Earth, 0.75f),
+                (ElementType.Air_Ice, 0.75f),
+                (ElementType.Air_Lightning, 0.75f),
+                (ElementType.Water_Life, 0.90f),
+                (ElementType.Water_Poision, 0.90f),
+                (ElementType.Earth_Steel, 0.75f),
+                (ElementType.Earth_Crystal, 0.75f)
+            ));
         }
     }
 }
