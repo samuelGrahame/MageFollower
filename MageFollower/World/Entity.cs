d63ef72e301ed8bf7159cf34f8f4f4df18ae1323
samuel grahame <samuel@intelogy.com.au>
2021-05-22 15:39:17 +1000

    added ELement type to entity

--- MageFollower/World/Entity.cs
+++ MageFollower/World/Entity.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MageFollower.World.Element;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -8,8 +9,8 @@ namespace MageFollower.World
     {
         public string Id { get; set; }
         public double Health { get; set; }
+        public string Name { get; set; }
+        public ElementType ElementType { get; set; }
 
-
-        
     }
 }
