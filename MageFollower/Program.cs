4bef28f4075f693dd906a316ceb227cbd3296c42
samuel grahame <samuel@intelogy.com.au>
2021-05-26 19:37:15 +1000

    aa

--- MageFollower/Program.cs
+++ MageFollower/Program.cs
@@ -1,4 +1,5 @@
-﻿using MageFollower.World;
+﻿using MageFollower.Client;
+using MageFollower.World;
 using MageFollower.World.Element;
 using MageFollower.World.Skills;
 using System;
@@ -10,70 +11,73 @@ namespace MageFollower
     {
         static void Main(string[] args)
         {
-            var r = new Random();
-            var fireMelee = new Entity() { 
-                ElementType = ElementType.Fire,
-                Health = 100,
-                Name = "The Fire Swordsman"
-            };
-            fireMelee.Melee.AddXp(10000.0f);            
-            fireMelee.RightHand = new World.Items.Item()
-            {
-                Equipt = World.Items.EquiptType.Physical,
-                Power = 1.1f,
-                Type = World.Items.ItemType.Stick
-            };
+            using (var game = new Game1())
+                game.Run();
 
-            var waterMelee = new Entity()
-            {
-                ElementType = ElementType.Water,
-                Health = 100,
-                Name = "The Water Swordsman"
-            };
-            waterMelee.Melee.AddXp(10000.0f);
-            waterMelee.RightHand = new World.Items.Item()
-            {
-                Equipt = World.Items.EquiptType.Physical,
-                Power = 1.1f,
-                Type = World.Items.ItemType.Stick
-            };
+            //var r = new Random();
+            //var fireMelee = new Entity() { 
+            //    ElementType = ElementType.Fire,
+            //    Health = 100,
+            //    Name = "The Fire Swordsman"
+            //};
+            //fireMelee.Melee.AddXp(10000.0f);            
+            //fireMelee.RightHand = new World.Items.Item()
+            //{
+            //    Equipt = World.Items.EquiptType.Physical,
+            //    Power = 1.1f,
+            //    Type = World.Items.ItemType.Stick
+            //};
 
-            if(r.NextDouble() < 0.5f)
-            {
-                while (waterMelee.IsAlive && fireMelee.IsAlive)
-                {
-                    fireMelee.AttackTarget(waterMelee,
-                        r.NextDouble());
-                    Thread.Sleep(100);
-                    if (waterMelee.IsAlive)
-                    {
-                        waterMelee.AttackTarget(fireMelee,
-                            r.NextDouble());
+            //var waterMelee = new Entity()
+            //{
+            //    ElementType = ElementType.Water,
+            //    Health = 100,
+            //    Name = "The Water Swordsman"
+            //};
+            //waterMelee.Melee.AddXp(10000.0f);
+            //waterMelee.RightHand = new World.Items.Item()
+            //{
+            //    Equipt = World.Items.EquiptType.Physical,
+            //    Power = 1.1f,
+            //    Type = World.Items.ItemType.Stick
+            //};
 
-                        Thread.Sleep(100);
-                    }
-                }
-            }
-            else
-            {
-                while (waterMelee.IsAlive && fireMelee.IsAlive)
-                {
-                    waterMelee.AttackTarget(fireMelee,
-                        r.NextDouble());
-                    Thread.Sleep(100);
-                    if (fireMelee.IsAlive)
-                    {
-                        fireMelee.AttackTarget(waterMelee,
-                            r.NextDouble());
+            //if(r.NextDouble() < 0.5f)
+            //{
+            //    while (waterMelee.IsAlive && fireMelee.IsAlive)
+            //    {
+            //        fireMelee.AttackTarget(waterMelee,
+            //            r.NextDouble());
+            //        Thread.Sleep(100);
+            //        if (waterMelee.IsAlive)
+            //        {
+            //            waterMelee.AttackTarget(fireMelee,
+            //                r.NextDouble());
 
-                        Thread.Sleep(100);
-                    }
-                }
-            }
-            
+            //            Thread.Sleep(100);
+            //        }
+            //    }
+            //}
+            //else
+            //{
+            //    while (waterMelee.IsAlive && fireMelee.IsAlive)
+            //    {
+            //        waterMelee.AttackTarget(fireMelee,
+            //            r.NextDouble());
+            //        Thread.Sleep(100);
+            //        if (fireMelee.IsAlive)
+            //        {
+            //            fireMelee.AttackTarget(waterMelee,
+            //                r.NextDouble());
 
-            Console.WriteLine($"The winner is: {(waterMelee.Health > 0 ? waterMelee.Name : fireMelee.Name)}");
-            Console.Read();
+            //            Thread.Sleep(100);
+            //        }
+            //    }
+            //}
+
+
+            //Console.WriteLine($"The winner is: {(waterMelee.Health > 0 ? waterMelee.Name : fireMelee.Name)}");
+            //Console.Read();
         }
     }
 }
